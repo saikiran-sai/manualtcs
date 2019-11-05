3a3af25702756fed5c739a252231ff69b6894219
sakiran-sai <saikiran.k0441@gmail.com>
2019-11-05 09:44:03 +0530

    adding testcases into git rep

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	@org.testng.annotations.Test
+	public void test22(){
+		System.out.println("haii");
+	}
+	@org.testng.annotations.Test
+	public void test11(){
+		
+		
+		System.out.println(10+20+30+"hai");
+		
+	}
+	
+
+}
